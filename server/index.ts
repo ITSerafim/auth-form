1e7049aa95fe0390d557b495e3a3cfc6376291fa
ITSerafim <aserafimow@gmail.com>
2023-08-10 11:00:49 +0300

    server init

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+async function bootstrap(): Promise<void> {
+    try {
+        app.listen(port, () => {
+            console.log(`Server started at http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+bootstrap();
