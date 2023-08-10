cc6aec370a081722c337cbd3e9d2ecfe9801a1c2
ITSerafim <aserafimow@gmail.com>
2023-08-10 12:14:04 +0300

    Добавил роутинг для авторизации

--- server/index.ts
+++ server/index.ts
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import db from './config/db';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.use('/api', router)
 
 async function bootstrap(): Promise<void> {
     await db.authenticate();
