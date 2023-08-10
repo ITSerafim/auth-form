c792d09161931d99c3dd05843de5f15c024d5100
ITSerafim <aserafimow@gmail.com>
2023-08-10 11:36:47 +0300

    Подключил базу данных

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import db from './config/db';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ app.use(cors());
 
 
 async function bootstrap(): Promise<void> {
+    await db.authenticate();
     try {
         app.listen(port, () => {
             console.log(`Server started at http://localhost:${port}`);
