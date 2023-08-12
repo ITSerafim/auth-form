51c9a89ca6a7013f7c6a6f6b5e544f2d5415a55d
ITSerafim <aserafimow@gmail.com>
2023-08-12 10:38:39 +0300

    Добавил eslint и prettier, и установил новые библиотеки

--- auth-react/src/App.tsx
+++ auth-react/src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import FormLayout from "./layouts/FormLayout";
-import FormBody from "./components/forms/auth/FormBody";
-import FormFooter from "./components/forms/auth/FormFooter";
-import FormHeader from "./components/forms/auth/FormHeader";
-import MainLayout from "./layouts/MainLayout";
+import React from 'react';
+import FormLayout from './layouts/FormLayout';
+import FormBody from './components/forms/auth/FormBody';
+import FormFooter from './components/forms/auth/FormFooter';
+import FormHeader from './components/forms/auth/FormHeader';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
